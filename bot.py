e75a76c188f531b9f08edc300504ee6e68f80a72
HarryLudemann <60420781+HarryLudemann@users.noreply.github.com>
2021-05-31 18:05:16 +1200

    Herokup Updates

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@ import os # for loading cog files
 import discord
 from discord.ext import commands
 from discord.utils import get
-from decouple import config #Get .env Varible
 # Custom Modules
 import functions
 
